4f8ec5aaae4ddbf6229856ad6a0b9c62852809af
Cooper <cooper.healy@nabis.com>
2025-04-09 12:42:33 -0500

    Disable ChatGPT
    For whatever reason, xenodium/chatgpt-shell doesn't want to cooperate
    with the API key. Rather than spend a bunch of time debugging it, I
    think it's best if I just disable it until I'm ready to deal with it.
    It's not like I use this feature much anyway

--- lisp/ai.el
+++ lisp/ai.el
@@ -10,19 +10,19 @@
             :rev :newest
             :branch "main"))
 
-(setq starmacs/chatgpt-api-key (expand-file-name "chatgpt-api-key.txt"))
-
-(use-package chatgpt-shell
-  :ensure t
-  :vc (:url "https://github.com/xenodium/chatgpt-shell")
-  :config
-  (unless
-      (file-exists-p starmacs/chatgpt-api-key)
-    (make-empty-file starmacs/chatgpt-api-key))
-
-  (setq chatgpt-shell-openai-key (replace-regexp-in-string "\n\\'" "" (with-temp-buffer
-                                                                        (insert-file-contents (expand-file-name "chatgpt-api-key.txt"))
-                                                                        (buffer-string)))))
+;; (setq starmacs/chatgpt-api-key (expand-file-name "chatgpt-api-key.txt"))
+;;
+;; (use-package chatgpt-shell
+;;   :ensure t
+;;   :vc (:url "https://github.com/xenodium/chatgpt-shell")
+;;   :config
+;;   (unless
+;;       (file-exists-p starmacs/chatgpt-api-key)
+;;     (make-empty-file starmacs/chatgpt-api-key))
+;;
+;;   (setq chatgpt-shell-openai-key (replace-regexp-in-string "\n\\'" "" (with-temp-buffer
+;;                                                                         (insert-file-contents (expand-file-name "chatgpt-api-key.txt"))
+;;                                                                         (buffer-string)))))
 
 (provide 'ai)
 ;;; ai.el ends here
