8b1b9ba7ba1a293f449de40e4a88c642346755a7
M Cooper Healy <m.cooper.healy@gmail.com>
2026-03-20 11:45:12 -0500

    Add squeak theme, add indent guides

--- lisp/ui-tweaks.el
+++ lisp/ui-tweaks.el
@@ -27,10 +27,16 @@
 ;;   :config
 ;;   (load-theme 'acme t))
 
-(use-package doom-themes
+(use-package squeak-theme
+  :vc (:url "https://github.com/isomatter-labs/squeak-theme" :rev :newest)
   :ensure t
   :config
-  (load-theme 'doom-one))
+  (load-theme 'squeak t))
+
+;; (use-package doom-themes
+;;   :ensure t
+;;   :config
+;;   (load-theme 'doom-one))
 
 (use-package base-line
   :vc (:url "https://github.com/isomatter-labs/base-line" :rev :newest)
@@ -90,6 +96,22 @@
 ;; Ensure that comments are italic to further distinguish them
 (set-face-italic 'font-lock-comment-face t)
 (set-face-italic 'font-lock-doc-face t)
+
+;; Provide indent guides for whitespace-dependent languages
+(use-package indent-bars
+  :custom
+  (indent-bars-pattern ". . . . ")
+  (indent-bars-width-frac 0.25)
+  (indent-bars-pad-frac 0.2)
+  (indent-bars-zigzag 0.1)
+  (indent-bars-color-by-depth '(:regexp "outline-\\([0-9]+\\)" :blend 1))
+  (indent-bars-highlight-current-depth '(:pattern "." :pad 0.1 :width 0.45))
+  (indent-bars-no-descend-lists 'skip) ; prevent extra bars in nested lists + skip intermediate bars
+  (indent-bars-treesit-support t)
+  (indent-bars-treesit-ignore-blank-lines-types '("module"))
+  (indent-bars-treesit-scope '((python function_definition class_definition for_statement
+                                       if_statement with_statement while_statement)))
+  :hook ((python-base-mode yaml-mode) . indent-bars-mode))
 
 (provide 'ui-tweaks)
 ;;; ui-tweaks.el ends here
