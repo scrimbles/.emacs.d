9ae8702b0b41a5783c2f9412afce6b2a70623673
M Cooper Healy <cooper.healy@nabis.com>
2026-04-24 13:11:07 -0500

    Live the lisp machine dream

--- lisp/ui-tweaks.el
+++ lisp/ui-tweaks.el
@@ -86,7 +86,7 @@
   :config
   (set-face-attribute 'dont-talk-to-computers-face nil
                       ;; https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/DepartureMono.zip
-                      :font "DepartureMono Nerd Font Mono"
+                      :font "Interlisp"
                       :height 110)
   (dont-talk-to-computers-mode 1))
 
